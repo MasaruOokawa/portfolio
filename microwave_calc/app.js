b4972e7ad2fe938e4f6ed8046f1d3dee552d5cc7
オオカワ マサル <117055065+MasaruOokawa@users.noreply.github.com>
2023-03-02 14:07:10 +0900

    Add files via upload

--- microwave_calc/app.js
+++ microwave_calc/app.js
@@ -3,7 +3,7 @@ function mw_calc() {
   b_sec = document.getElementById("b_sec").value; // パッケージ記載の調理時間（秒）省略不可。60以上の値を指定しても良い
   b_w1 = document.getElementById("b_w1").value; // パッケージ記載のワット数
   b_w2 = document.getElementById("b_w2").value; // 電子レンジの持つワット数
-
+  b_qty = document.getElementById("b_qty").value; // 個数
   if (b_min == "") {
     isec1 = Number(b_sec);
   }
@@ -13,11 +13,13 @@ function mw_calc() {
 
   w1 = Number(b_w1);
   w2 = Number(b_w2);
-
+  qty = Number(b_qty);
+  
   // 電子レンジが与えるべきエネルギー量として、ワット秒を求める
-  ws = w1 * isec1;
+  ws = w1 * isec1 * qty;
 
   // ワット秒を、電子レンジの持つワット数で割り、秒を求める(小数点以下切り捨て)
   isec2 = ws / w2;
+
   document.getElementById("result").innerHTML = "<b>調理時間：" + Math.floor(isec2) + "秒 (" + Math.floor(isec2 / 60) + "分" + Math.floor(isec2 % 60) + "秒)</b>";
 }
