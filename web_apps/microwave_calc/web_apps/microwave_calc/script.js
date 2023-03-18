ab3f82946af100363ba3f94dac137902b1637ee9
オオカワ マサル <117055065+MasaruOokawa@users.noreply.github.com>
2023-03-18 16:52:11 +0900

    Add files via upload

---
+++ web_apps/microwave_calc/web_apps/microwave_calc/script.js
@@ -0,0 +1,5 @@
+  // add extra elements to buttons for hover, keeps html cleaner
+  $('.btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span><span></span></div>');
+
+//   $('.social-btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span></div>');
+  
